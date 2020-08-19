7cd683afd4ad341e279de67cd7685306237016d3
katylurdht <katyhuamanitito1@gmail.com>
2020-08-19 08:33:09 -0500

    ageregando carpeta webfonts

--- src/main.js
+++ src/main.js
@@ -3,4 +3,20 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// console.log(example, data);
+
+
+
+function poketemplate(pokemon){
+    return `
+    <div>
+    <p>${pokemon.num}</p>
+    <img src="${pokemon.img}">
+    <p>${pokemon.name}</p>
+    </div>
+    `
+}
+document.getElementById("imagenes").innerHTML = `
+<h1>pokemon ${data.pokemon.length}pokemon</h1>
+${data.pokemon.map(poketemplate).join('')}
+`

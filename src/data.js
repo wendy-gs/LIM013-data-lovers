0fd4f9020f096491298198fa3d8a7ed5ce1eb590
katylurdht <katyhuamanitito1@gmail.com>
2020-08-21 10:34:45 -0500

    agregue combobo html

--- src/data.js
+++ src/data.js
@@ -8,3 +8,21 @@ export const anotherExample = () => {
   return 'OMG';
 };
 
+
+// sortData: (dataPokemon, sortBy, sortOrder) => {
+//   if (sortBy === 'name') {
+//     if (sortOrder === true){
+//         return dataPokemon.slice().sort((a, b) => {
+//             let x = a[sortBy].toLowerCase();
+//             let y = b[sortBy].toLowerCase();
+//             if (x < y) {
+//                 return -1;
+//             } if (x > y) {
+//                 return 1;
+//             } else {
+//                 return 0;
+//             }
+//           }
+//         }
+//       }
+//         };
--- src/main.js
+++ src/main.js
@@ -1,22 +1,51 @@
+/* eslint-disable no-undef */
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+// import funciones from './data.js';
 
-// console.log(example, data);
+console.log(example, data);
+
+const POKEMON_API = './data/pokemon/pokemon.json';
+
+const promise = fetch(POKEMON_API);
 
 
 
-function poketemplate(pokemon){
-    return `
+function mostrandoPoke(poke){
+return `
     <div>
-    <p>${pokemon.num}</p>
-    <img src="${pokemon.img}">
-    <p>${pokemon.name}</p>
+    <p>${poke.num}</p>
+    <img src="${poke.img}">
+    <p>${poke.name}</p>
     </div>
-    `
-}
-document.getElementById("imagenes").innerHTML = `
-<h1>pokemon ${data.pokemon.length}pokemon</h1>
-${data.pokemon.map(poketemplate).join('')}
 `
+}
+document.getElementById("imagenes").innerHTML=`
+<p>tenemos  ${data.pokemon.length}pokemon</p>
+${data.pokemon.map(mostrandoPoke).join('')}`
+
+// // 
+
+
+// fillWithPokemons(allPokemon);
+
+// // aqui ordenamos de ascendente y descendente
+
+// const allPokemon = window.POKEMON.pokemon;
+// let currentPokemon = allPokemon;
+
+// let selectSort = document.getElementById("sort list");
+// selectSort.addEventListener("change", () => {
+//   let valueSelect = selectSort.value;
+//   let pokemonsOrdered = [];
+//   if(valueSelect === 'upward'){
+//     pokemonsOrdered = window.pokemons.sortData(currentPokemon, 'name', true);
+//   }else if(valueSelect === 'falling'){
+//     pokemonsOrdered = window.pokemons.sortData(currentPokemon, 'name', false);
+//   }else{
+//     pokemonsOrdered = currentPokemon;
+// }
+// fillWithPokemons(pokemonsOrdered);
+// });
